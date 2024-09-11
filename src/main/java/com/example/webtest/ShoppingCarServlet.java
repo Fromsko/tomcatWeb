a2c6200aec47b6eaeb778fbeac5fc7478176a761
Fromsko <1614355756@qq.com>
2024-09-11 19:51:56 +0800

    feat: 增加购入选择

---
+++ src/main/java/com/example/webtest/ShoppingCarServlet.java
@@ -0,0 +1,37 @@
+package com.example.webtest;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "ShoppingCarServlet", value = "/ShoppingCarServlet")
+public class ShoppingCarServlet extends HttpServlet {
+
+    @Override
+    public void init() {
+        System.out.println(this.getServletName() + "的 init() 方法被调用");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        try {
+            super.doGet(request, response);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
